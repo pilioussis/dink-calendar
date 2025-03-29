093aa36bca79ff37cf2bb12139eccf059ba17bf2
Dean Pilioussis <dean.pilioussis@gmail.com>
2025-03-29 16:28:26 +1100

    Updated obama styling

--- calendar/render_device.sh
+++ calendar/render_device.sh
@@ -3,8 +3,8 @@ set -e
 echo "Create bin"
 docker compose exec dink go build -o dink_bin main/**.go
 
-# echo "Copy bin to pi"
-# scp ./dink_bin dink:/home/dink/prog/dink-calendar/calendar
+echo "Copy bin to pi"
+scp ./dink_bin dink:/home/dink/prog/dink-calendar/calendar
 
 # echo "Run"
 # ssh dink 'cd /home/dink/prog/dink-calendar/calendar && ./dink_bin serve'
