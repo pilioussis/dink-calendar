740eeed9ed5053f2da5673a49169185ea1567fa5
Dean Pilioussis <dean.pilioussis@gmail.com>
2025-03-29 16:30:47 +1100

    fixed run script

--- calendar/run.sh
+++ calendar/run.sh
@@ -1,9 +1,4 @@
 set -e
 
 cd /home/dink/prog/dink-calendar/calendar && ./dink_bin
-
-cp ./out/dither.bmp /home/dink/prog/13.3inch_e-Paper_E/RaspberryPi/python/pic/
-
-cd /home/dink/prog/13.3inch_e-Paper_E/RaspberryPi/python/examples && python epd_13in3E_test.py
-
-# nohup ./dink_bin serve > /home/dink/log/dink.log 2>&1 &
+nohup ./dink_bin serve > /home/dink/log/dink.log 2>&1 &
